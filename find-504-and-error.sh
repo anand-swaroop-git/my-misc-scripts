8b75716afa855d995a497bc363a680e3cf1c62dd
Anand_Swaroop <prashu.swaroop@gmail.com>
2018-09-27 03:09:03 +0530

    Added php-fpm logs - testing still needed

--- find-504-and-error.sh
+++ find-504-and-error.sh
@@ -1,15 +1,40 @@
 clear
 IFS=\n
-for date in {02..04}
+for date in {25..26}
 do
-echo -e "\e[1;32mNumber of 504s in Access logs on $date/Sep\e[0m"
+echo -e "\e[1;32mAccess logs 504s on $date/Sep - Location (/var/log/nginx/)e[0m"
 zgrep "$date/Sep/2018" /var/log/nginx/access.log.* | grep " 504 "
     IFS=" "
     for times in `zgrep "$date/Sep/2018" /var/log/nginx/access.log.* | grep " 504 " | awk '{print $4}' | awk -F: '{print $2" "$3}' | tr " " ":" | tr "\n" " "`
     do
-    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-    echo -e "\e[1;32mNow the error logs corresponding to the access logs 504\e[0m"
+    # echo "--------------------------------------------------------------------------------------------------------"
+    echo -e "\e[1;31mError logs on $date/Sep $times - Location (/var/log/nginx/)\e[0m"
     zgrep "2018/09/$date $times" /var/log/nginx/error.log.*
+    # echo -e "\e[1;36mphp5-fpm logs on $date/Sep $times - Location (/var/log/)\e[0m"
+    # zgrep "$date-Sep-2018 $times" /var/log/php5-fpm.log.*
     done
-echo "************************************************************************************************************"
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 done
+
+# Added php-fpm logs for a specific day and not the timestamp
+clear
+IFS=\n
+for date in {20..26}
+do
+echo -e "\e[1;32mAccess logs 504s on $date/Sep - Location /var/log/nginx/ e[0m"
+zgrep "$date/Sep/2018" /var/log/nginx/access.lo* | grep " 504 "
+    IFS=" "
+    for times in `zgrep "$date/Sep/2018" /var/log/nginx/access.lo* | grep " 504 " | awk '{print $4}' | awk -F: '{print $2" "$3}' | tr " " ":" | tr "\n" " "`
+    do
+    # echo "--------------------------------------------------------------------------------------------------------"
+    echo -e "\e[1;31mError logs on $date/Sep $times - Location (/var/log/nginx/)\e[0m"
+    zgrep "2018/09/$date $times" /var/log/nginx/error.lo*
+    echo -e "\e[1;36mphp5-fpm logs on $date/Sep - Location /var/log/ \e[0m"
+    zgrep "$date-Sep-2018" /var/log/php5-fpm.lo*
+    done
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+done
+
+
+
+
