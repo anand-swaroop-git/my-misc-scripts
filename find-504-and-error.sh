c019dea7a4bbcef9d9f3c9d659cdd17113ad774d
Anand_Swaroop <prashu.swaroop@gmail.com>
2018-09-27 02:37:43 +0530

    Script working fine with color coding

--- find-504-and-error.sh
+++ find-504-and-error.sh
@@ -7,7 +7,7 @@ zgrep "$date/Sep/2018" /var/log/nginx/access.log.* | grep " 504 "
     IFS=" "
     for times in `zgrep "$date/Sep/2018" /var/log/nginx/access.log.* | grep " 504 " | awk '{print $4}' | awk -F: '{print $2" "$3}' | tr " " ":" | tr "\n" " "`
     do
-    echo "--------------------------------------------------------------------------------------------------------"
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
     echo -e "\e[1;32mNow the error logs corresponding to the access logs 504\e[0m"
     zgrep "2018/09/$date $times" /var/log/nginx/error.log.*
     done
