6f197e0123e940e46e83c520f12bf05710194ac5
payne <prashu.swaroop@email.com>
2018-10-13 17:59:21 +0530

    Script is working fine with 504 in access, error and php5-fpm logs showing correct results

--- find-504-and-error.sh
+++ find-504-and-error.sh
@@ -1,22 +1,4 @@
-clear
-IFS=\n
-for date in {25..26}
-do
-echo -e "\e[1;32mAccess logs 504s on $date/Sep - Location (/var/log/nginx/)e[0m"
-zgrep "$date/Sep/2018" /var/log/nginx/access.log.* | grep " 504 "
-    IFS=" "
-    for times in `zgrep "$date/Sep/2018" /var/log/nginx/access.log.* | grep " 504 " | awk '{print $4}' | awk -F: '{print $2" "$3}' | tr " " ":" | tr "\n" " "`
-    do
-    # echo "--------------------------------------------------------------------------------------------------------"
-    echo -e "\e[1;31mError logs on $date/Sep $times - Location (/var/log/nginx/)\e[0m"
-    zgrep "2018/09/$date $times" /var/log/nginx/error.log.*
-    # echo -e "\e[1;36mphp5-fpm logs on $date/Sep $times - Location (/var/log/)\e[0m"
-    # zgrep "$date-Sep-2018 $times" /var/log/php5-fpm.log.*
-    done
-printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-done
-
-# Added php-fpm logs for a specific day and not the timestamp
+# Script finds the 504s in the access logs (across zipped access logs as well), greps the timestamp of the log files and then finds the error logs (Across zipped error logs as well) corresponding to the timestamps of 504's access log entries found.
 clear
 IFS=\n
 for date in {20..26}
@@ -26,7 +8,6 @@ zgrep "$date/Sep/2018" /var/log/nginx/access.lo* | grep " 504 "
     IFS=" "
     for times in `zgrep "$date/Sep/2018" /var/log/nginx/access.lo* | grep " 504 " | awk '{print $4}' | awk -F: '{print $2" "$3}' | tr " " ":" | tr "\n" " "`
     do
-    # echo "--------------------------------------------------------------------------------------------------------"
     echo -e "\e[1;31mError logs on $date/Sep $times - Location (/var/log/nginx/)\e[0m"
     zgrep "2018/09/$date $times" /var/log/nginx/error.lo*
     echo -e "\e[1;36mphp5-fpm logs on $date/Sep - Location /var/log/ \e[0m"
